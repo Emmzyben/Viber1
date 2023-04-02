c385539080dc61a5dd2132af2cccb3d4e6654885
Emmzyben <117511921+Emmzyben@users.noreply.github.com>
2023-04-02 05:26:23 +0100

    Update contractUtils.js

--- contractUtils.js
+++ contractUtils.js
@@ -1,4 +1,4 @@
 
-    CONTRACT_ADDRESS = "0x644d1a4f468649E919E3fbcd2B31c342C3e8b8De"
+    CONTRACT_ADDRESS = "0x3DA16539399a9d98816ED2988AE2f598Add6D77B"
     
-    Verification status =https://mumbai.polygonscan.com/address/0x644d1a4f468649E919E3fbcd2B31c342C3e8b8De
+    Verification status =https://mumbai.polygonscan.com/address/0x3DA16539399a9d98816ED2988AE2f598Add6D77B
